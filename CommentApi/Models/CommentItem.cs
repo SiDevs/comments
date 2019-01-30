51711188eab3fb7bbd0e729d4242dc062b0ca389
SiDev <welsh51@tiscali.co.uk>
2019-01-30 09:39:13 +0000

    added MongoDB CRUD Api

--- CommentApi/Models/CommentItem.cs
+++ CommentApi/Models/CommentItem.cs
@@ -1,9 +1,26 @@
-﻿namespace CommentApi.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CommentApi.Models
 {
     public class CommentItem
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsPublic { get; set; }
+        [BsonId]                                    // Designates as document primary key
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName { get; set; }
+
+        [BsonElement("IsPublic")]
+        public string IsPublic { get; set; }
+
     }
+
+    //public class CommentItem
+    //{
+    //    public long Id { get; set; }
+    //    public string Name { get; set; }
+    //    public bool IsPublic { get; set; }
+    //}
 }
